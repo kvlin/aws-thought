cfacff4a8314fa949ab86f22abc832ea489125c0
kvlin <kevlin129@hotmail.com>
2021-08-25 17:58:32 +1000

    Adding more routes

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -29,5 +29,68 @@ router.get('/users', (req, res) => {
     })
 })
 
+// Route to return all thoughts from a particular user
+router.get('/users/:username', (req, res) => {
+    console.log(`Querying for thought(s) from ${req.params.username}.`);
+
+    // params to define the query call to DynamoDB
+    const params = {
+        TableName:table,
+        // define aliases with # prefix - best practice to avoid list of reserved words like 'date', 'time', 'user', 'data'
+        ExpressionAttributeNames: { 
+            "#un": "username",
+            "#ca": "createdAt",
+            "#th": "thought"
+        },
+        // values use aliases with : prefix 
+        ExpressionAttributeValues: {
+            ":user": req.params.username 
+        },
+        // specifies the search criteria
+        KeyConditionExpression: "#un = :user", 
+        // determines which attributes (or columns) will be returned
+        ProjectionExpression: "#th, #ca",
+        // specifies the order of the sort key - default to true (ascending)
+        // we want decending here for most recent thoughts on top
+        ScanIndexForward: false
+    }
+
+    // query method for collection of items(the scan method scans the whole table - more time, 
+    // query method directly looks up based on primary or secondary partition/hash key )
+    dynamodb.query(params, (err, data) => {
+        if (err) {
+          console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+          res.status(500).json(err); // an error occurred
+        } else {
+          console.log("Query succeeded.");
+          res.json(data.Items)
+        }
+    });
+
+});
+
+// Create new user at /api/users
+// Create new user
+router.post('/users', (req, res) => {
+    const params = {
+      TableName: table,
+      Item: {
+        "username": req.body.username,
+        "createdAt": Date.now(),
+        "thought": req.body.thought
+      }
+    };
+    dynamodb.put(params, (err, data) => {
+      if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        res.status(500).json(err); // an error occurred
+      } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+        res.json({"Added": JSON.stringify(data, null, 2)});
+      }
+    });
+  });
+
+
 module.exports = router;
 
