ecdee8eb9cf40d1689d3a2dcfce119ac206266ca
kvlin <kevlin129@hotmail.com>
2021-09-07 22:47:43 +1000

    Home page UI update

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { GithubOutlined } from '@ant-design/icons'
 
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container">&copy;2020 by Lernantino</div>
+      <div className="container"><a href="http://https://github.com/kvlin/aws-thought"><GithubOutlined /> kvlin</a></div>
     </footer>
   );
 };
